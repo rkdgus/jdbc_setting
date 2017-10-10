d079810bba426d73ca80ccd0825cc0a8fe5938a8
KangHyun Lee <rkd7327@naver.com>
2017-10-10 15:04:58 +0900

    Connection test

--- src/kr/or/dgit/jdbc_setting/TestMain.java
+++ src/kr/or/dgit/jdbc_setting/TestMain.java
@@ -1,11 +1,16 @@
 package kr.or.dgit.jdbc_setting;
 
+import java.sql.Connection;
+
 import kr.or.dgit.jdbc_setting.jdbc.DBCon;
 
 public class TestMain {
 
 	public static void main(String[] args) {
-		DBCon.getInstance();
+		DBCon dbCon = DBCon.getInstance();
+		
+		Connection connection  = dbCon.getConnection();
+		System.out.println(connection);
 
 	}
 
--- src/kr/or/dgit/jdbc_setting/jdbc/DBCon.java
+++ src/kr/or/dgit/jdbc_setting/jdbc/DBCon.java
@@ -3,6 +3,8 @@ package kr.or.dgit.jdbc_setting.jdbc;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBCon {
@@ -16,9 +18,15 @@ public class DBCon {
 	
 	private DBCon(){
 		Properties properties = getProperties("conf.properties");
-		System.out.println(properties.getProperty("user"));
+		try {
+			connection = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("user"), properties.getProperty("pwd"));
+		} catch (SQLException e) {
+			System.err.printf("%s - %s%n",e.getErrorCode(),e.getMessage());
+			e.printStackTrace();
+		}
+		/*System.out.println(properties.getProperty("user"));
 		System.out.println(properties.getProperty("pwd"));
-		System.out.println(properties.getProperty("url"));
+		System.out.println(properties.getProperty("url"));*/
 	}
 	
 	private Properties getProperties(String propertiesPath) {
